5440fcdcd9c4016b313286fcbbd68b178ddd817c
opsatya <satyjeet.singh12005@gmail.com>
2025-07-31 09:47:21 +0530

    learn about the array and the arraylist

---
+++ Arrays-ArrayList/src/main/java/com/webvitels/ArraylistExample.java
@@ -0,0 +1,10 @@
+package com.webvitels;
+
+import java.util.ArrayList;
+
+public class ArraylistExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+    }
+}
